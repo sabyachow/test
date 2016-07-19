79d189e22c8fcebc8105322a1836384765110fa0
Sabyasachi Chowdhury <sabya_chow@yahoo.co.in>
2016-07-19 11:57:44 -0400

    initial project version

---
+++ src/com/voya/rest/sample/client/SampleRestClient.java
@@ -0,0 +1,14 @@
+package com.voya.rest.sample.client;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ctsuser1
+ * Date: 12/17/15
+ * Time: 5:52 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class SampleRestClient {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
